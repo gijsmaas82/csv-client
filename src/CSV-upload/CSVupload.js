19306bb827a3834c4c9f67075178ede3ab8e48cf
gijsmaas82 <gijsmaas82@outlook.com>
2020-01-21 13:45:34 +0100

    Data is shown on screen

--- src/CSV-upload/CSVupload.js
+++ src/CSV-upload/CSVupload.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Form, Button, Table } from 'react-bootstrap'
 
 export default class CSVupload extends Component {
   state = {
-    selectedFile: null
+    selectedFile: null,
+    fileUploaded: false,
+    data: []
   }
 
 onChangeHandler=event=>{
@@ -15,22 +18,67 @@ onClickHandler = () => {
   const data = new FormData()
   data.append('file', this.state.selectedFile)
   axios.post("http://localhost:5000/upload", data)
-  .then(res => { 
-    console.log(res.statusText)
+  .then(res => {
+    if (res.statusText === 'OK') {
+      this.setState({
+        fileUploaded: true
+      })
+    }  
   })
+}
+
+getData = () => {
+  axios.get("http://localhost:5000/data")
+    .then(res => {
+      // console.log(res.data)
+      this.setState({
+        data: res.data
+      })
+    })
+    .catch(console.error)
 }
   render() {
     return (
-      <div class="container">
-        <div class="row">
-          <div class="offset-md-3 col-md-6">
-            <div class="form-group files">
-              <label>upload your file</label>
-              <input type="file" name="file" onChange={this.onChangeHandler}/>
-            </div>
-            <button type="button" onClick={this.onClickHandler}>Upload</button>
-          </div>
+      <div>
+        { !this.state.fileUploaded ?
+        <Form>
+          <Form.Group>
+            <Form.Label>Upload Data</Form.Label>
+            <Form.Control type="file" name="file" onChange={this.onChangeHandler}/>
+          </Form.Group>
+          <Button variant="primary" onClick={this.onClickHandler}>Upload</Button>
+        </Form> :
+        <div>
+        <h2>Click on the button to get the data</h2>
+        <Button variant="primary" onClick={this.getData}>Get Data</Button>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <td>id</td>
+              <td>level</td>
+              <td>cvss</td>
+              <td>title</td>
+              <td>Vulnerability</td>
+              <td>Solution</td>
+              <td>reference</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(row => {
+              return <tr>
+                <td>{row.id}</td>
+                <td>{row.level}</td>
+                <td>{row.cvss}</td>
+                <td>{row.title}</td>
+                <td>{row.Vulnerability}</td>
+                <td>{row.Solution}</td>
+                <td>{row.reference}</td>
+              </tr>
+            })}
+          </tbody>
+        </Table>
         </div>
+        }
       </div>
     )
   }
